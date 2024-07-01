570b0eaa18fe6135518159f2541c9b573276eda2
julien.bouin.etu <julien.bouin.etu@univ-lille.fr>
2024-07-01 09:47:17 +0100

    init modules user and auth

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(),
+    AuthModule,
+    UserModule,
+  ],
+})
+export class AppModule { }
